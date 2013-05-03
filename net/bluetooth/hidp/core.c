ed01cd04153cd458deecb0150fedfe46677e30b8
Christopher R. Palmer <crpalmer@gmail.com>
2013-05-03 15:43:24 +0200

    bluetooth: bulk update to the CAF drivers

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -1,7 +1,7 @@
 /*
    HIDP implementation for Linux Bluetooth stack (BlueZ).
    Copyright (C) 2003-2004 Marcel Holtmann <marcel@holtmann.org>
-   Copyright (c) 2012 Code Aurora Forum.  All rights reserved.
+   Copyright (c) 2012 The Linux Foundation.  All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License version 2 as
@@ -101,7 +101,20 @@ static void __hidp_link_session(struct hidp_session *session)
 
 static void __hidp_unlink_session(struct hidp_session *session)
 {
-	if (session->conn)
+	bdaddr_t *dst = &session->bdaddr;
+	struct hci_dev *hdev;
+	struct device *dev = NULL;
+
+	hdev = hci_get_route(dst, BDADDR_ANY);
+	if (hdev) {
+		session->conn = hci_conn_hash_lookup_ba(hdev, ACL_LINK, dst);
+		if (session->conn)
+			dev = &session->conn->dev;
+
+		hci_dev_put(hdev);
+	}
+
+	if (dev)
 		hci_conn_put_device(session->conn);
 
 	list_del(&session->list);
@@ -201,10 +214,12 @@ static void hidp_input_report(struct hidp_session *session, struct sk_buff *skb)
 	int i, size = skb->len - 1;
 
 	switch (skb->data[0]) {
-	case 0x01:	
+	case 0x01:	/* Keyboard report */
 		for (i = 0; i < 8; i++)
 			input_report_key(dev, hidp_keycode[i + 224], (udata[0] >> i) & 1);
 
+		/* If all the key codes have been set to 0x01, it means
+		 * too many keys were pressed at the same time. */
 		if (!memcmp(udata + 2, hidp_mkeyspat, 6))
 			break;
 
@@ -227,7 +242,7 @@ static void hidp_input_report(struct hidp_session *session, struct sk_buff *skb)
 		memcpy(keys, udata, 8);
 		break;
 
-	case 0x02:	
+	case 0x02:	/* Mouse report */
 		input_report_key(dev, BTN_LEFT,   sdata[0] & 0x01);
 		input_report_key(dev, BTN_RIGHT,  sdata[0] & 0x02);
 		input_report_key(dev, BTN_MIDDLE, sdata[0] & 0x04);
@@ -364,20 +379,22 @@ static void hidp_process_handshake(struct hidp_session *session,
 
 	switch (param) {
 	case HIDP_HSHK_SUCCESSFUL:
-		
+		/* FIXME: Call into SET_ GET_ handlers here */
 		break;
 
 	case HIDP_HSHK_NOT_READY:
 	case HIDP_HSHK_ERR_INVALID_REPORT_ID:
 	case HIDP_HSHK_ERR_UNSUPPORTED_REQUEST:
 	case HIDP_HSHK_ERR_INVALID_PARAMETER:
-		
+		/* FIXME: Call into SET_ GET_ handlers here */
 		break;
 
 	case HIDP_HSHK_ERR_UNKNOWN:
 		break;
 
 	case HIDP_HSHK_ERR_FATAL:
+		/* Device requests a reboot, as this is the only way this error
+		 * can be recovered. */
 		__hidp_send_ctrl_message(session,
 			HIDP_TRANS_HID_CONTROL | HIDP_CTRL_SOFT_RESET, NULL, 0);
 		break;
@@ -395,11 +412,11 @@ static void hidp_process_hid_control(struct hidp_session *session,
 	BT_DBG("session %p param 0x%02x", session, param);
 
 	if (param == HIDP_CTRL_VIRTUAL_CABLE_UNPLUG) {
-		
+		/* Flush the transmit queues */
 		skb_queue_purge(&session->ctrl_transmit);
 		skb_queue_purge(&session->intr_transmit);
 
-		
+		/* Kill session thread */
 		atomic_inc(&session->terminate);
 		hidp_schedule(session);
 	}
@@ -610,7 +627,7 @@ static int hidp_session(void *arg)
 		session->hid = NULL;
 	}
 
-	
+	/* Wakeup user-space polling for socket errors */
 	session->intr_sock->sk->sk_err = EUNATCH;
 	session->ctrl_sock->sk->sk_err = EUNATCH;
 
@@ -950,15 +967,15 @@ int hidp_del_connection(struct hidp_conndel_req *req)
 			hidp_send_ctrl_message(session,
 				HIDP_TRANS_HID_CONTROL | HIDP_CTRL_VIRTUAL_CABLE_UNPLUG, NULL, 0);
 		} else {
-			
+			/* Flush the transmit queues */
 			skb_queue_purge(&session->ctrl_transmit);
 			skb_queue_purge(&session->intr_transmit);
 
-			
+			/* Wakeup user-space polling for socket errors */
 			session->intr_sock->sk->sk_err = EUNATCH;
 			session->ctrl_sock->sk->sk_err = EUNATCH;
 
-			
+			/* Kill session thread */
 			atomic_inc(&session->terminate);
 			hidp_schedule(session);
 		}
