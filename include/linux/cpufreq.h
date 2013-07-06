cdac2bc88cee84e6f9175a780a766c492b859c0e
tbalden <illespal@gmail.com>
2013-07-06 23:47:15 +0200

    cpufreq: adding intellidemand and dancedance governors
    intellidemand patch by @faux123
    dancedance patch by @Snuzzo

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dancedance)
 #endif
 
 
