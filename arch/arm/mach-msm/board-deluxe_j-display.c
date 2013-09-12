c46e31199c6ef7fd3bdf15549f7d8851fae59495
tako0910 <tako.oct0910@gmail.com>
2013-09-12 14:56:42 +0900

    Fix syntax and Add sony init 2

--- arch/arm/mach-msm/board-deluxe_j-display.c
+++ arch/arm/mach-msm/board-deluxe_j-display.c
@@ -909,6 +909,76 @@ static int __init mipi_video_sharp_init(void)
 	return ret;
 }
 
+static int __init mipi_video_sony_init(void)
+{
+	int ret;
+
+	pinfo.xres = 1080;
+	pinfo.yres = 1920;
+	pinfo.type = MIPI_VIDEO_PANEL;
+	pinfo.pdest = DISPLAY_1;
+	pinfo.wait_cycle = 0;
+	pinfo.bpp = 24;
+//	pinfo.width = 61;
+//	pinfo.height = 110;
+//	pinfo.camera_backlight = 176;
+
+	pinfo.lcdc.h_back_porch = 50;
+	pinfo.lcdc.h_front_porch = 100;
+	pinfo.lcdc.h_pulse_width = 10;
+	pinfo.lcdc.v_back_porch = 3;
+	pinfo.lcdc.v_front_porch = 3;
+	pinfo.lcdc.v_pulse_width = 2;
+
+	pinfo.lcd.v_back_porch = 3;
+	pinfo.lcd.v_front_porch = 3;
+	pinfo.lcd.v_pulse_width = 2;
+
+	pinfo.lcd.vsync_enable = TRUE;
+	pinfo.lcd.hw_vsync_mode = TRUE;
+	pinfo.lcd.primary_vsync_init = pinfo.yres;
+
+	pinfo.lcdc.border_clr = 0;	/* blk */
+	pinfo.lcdc.underflow_clr = 0xff;	/* blue */
+	pinfo.lcdc.hsync_skew = 0;
+	pinfo.bl_max = 255;
+	pinfo.bl_min = 1;
+	pinfo.fb_num = 2;
+	pinfo.clk_rate = 848000000;
+
+	pinfo.mipi.mode = DSI_VIDEO_MODE;
+	pinfo.mipi.pulse_mode_hsa_he = TRUE;
+	pinfo.mipi.hfp_power_stop = FALSE;
+	pinfo.mipi.hbp_power_stop = FALSE;
+	pinfo.mipi.hsa_power_stop = TRUE;
+	pinfo.mipi.eof_bllp_power_stop = TRUE;
+	pinfo.mipi.bllp_power_stop = TRUE;
+	pinfo.mipi.traffic_mode = DSI_NON_BURST_SYNCH_EVENT;
+	pinfo.mipi.dst_format = DSI_VIDEO_DST_FORMAT_RGB888;
+	pinfo.mipi.vc = 0;
+	pinfo.mipi.rgb_swap = DSI_RGB_SWAP_RGB;
+	pinfo.mipi.data_lane0 = TRUE;
+	pinfo.mipi.data_lane1 = TRUE;
+	pinfo.mipi.data_lane2 = TRUE;
+	pinfo.mipi.data_lane3 = TRUE;
+
+	pinfo.mipi.tx_eot_append = TRUE;
+	pinfo.mipi.t_clk_post = 0x02;
+	pinfo.mipi.t_clk_pre = 0x2C;
+	pinfo.mipi.stream = 0; /* dma_p */
+	pinfo.mipi.mdp_trigger = DSI_CMD_TRIGGER_SW;
+	pinfo.mipi.dma_trigger = DSI_CMD_TRIGGER_SW;
+	pinfo.mipi.frame_rate = 60;
+	pinfo.mipi.dsi_phy_db = &dsi_video_mode_phy_db;
+
+	ret = mipi_deluxe_j_device_register(&pinfo, MIPI_DSI_PRIM,
+						MIPI_DSI_PANEL_FWVGA_PT);
+	if (ret)
+		pr_err("%s: failed to register device!\n", __func__);
+
+	return ret;
+}
+
 static const struct i2c_device_id pwm_i2c_id[] = {
 	{ "pwm_i2c", 0 },
 	{ }
@@ -976,7 +1046,7 @@ static int __init deluxe_j_panel_init(void)
 	if (panel_type == PANEL_ID_DLXJ_SHARP_RENESAS) {
 		mipi_video_sharp_init();
 		printk(KERN_INFO "match PANEL_ID_DLXJ_SHARP_RENESAS panel_type\n");
-	else if (panel_type == PANEL_ID_DLXJ_SONY_RENESAS) {
+	} else if (panel_type == PANEL_ID_DLXJ_SONY_RENESAS) {
       		mipi_video_sony_init();
      		printk(KERN_INFO "match PANEL_ID_DLXJ_SONY_RENESAS panel_type\n");
 	} else {
