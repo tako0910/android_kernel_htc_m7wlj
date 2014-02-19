a58c7d3bb05cb5c2579000729334d3c802577a51
vroad <xvroad@gmail.com>
2014-02-19 20:44:49 +0900

    dlxj: Apply kexec-hardboot patch original patch was provided by jamiethemorris

--- arch/arm/mach-msm/board-deluxe_j.c
+++ arch/arm/mach-msm/board-deluxe_j.c
@@ -5137,6 +5137,17 @@ unsigned long ion_kgsl_heap_paddr = 0;
 
 static void __init deluxe_j_allocate_memory_regions(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page at the end of first system ram block
+	struct membank* bank = &meminfo.bank[0];
+	phys_addr_t start = bank->start + bank->size - SZ_1M;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 #ifdef CONFIG_FB_MSM
 	deluxe_j_allocate_fb_region();
 #endif
